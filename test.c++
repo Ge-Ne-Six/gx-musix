5a422dfc83d2fbeed1e5e47dc9794e4b695fab1e
Amaga Nnamdi joseph <138515295+Ge-Ne-Six@users.noreply.github.com>
2024-05-26 14:27:34 +0100

    by genesix

---
+++ test.c++
@@ -0,0 +1,26 @@
+#include  <iostream>
+using namespace std
+int main()
+{
+
+  int num;
+  cout << 'pls enter your age'
+  cin >> num
+
+  void age_limit_checker(age){
+
+    if(age >= 18){
+      cout << 'You are of age'
+    }else{
+      cout << 'You are not of age'
+    }
+
+  }
+
+  age_limit_checker(num)
+
+
+
+
+  return 0;
+}
---
+++ test.py
@@ -0,0 +1,35 @@
+# let num = 3;
+# let it = '';
+
+# function evenOddChecker(num){
+  
+#   if(num % 2 === 0){
+#     console.log('this number is an even number');
+#     it = 'this number is an even number'
+#   }else{
+#     console.log('this number is an odd number');
+#     it = 'this number is an odd number'
+#   }
+#   console.log(it)
+
+#   return it
+# }
+
+
+
+# evenOddChecker(num);
+
+# let myNum = 10;
+
+# evenOddChecker(myNum)
+
+
+num = input('pls enter a number')
+
+if num > 11 :
+  print('greater')
+
+else : 
+  print('lesser')
+
+
